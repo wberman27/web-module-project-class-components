2221f32d352cebc5bc426bceac5dcd8031511041
Will B <wberman27@gmail.com>
2021-03-08 21:11:23 -0500

    added some styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,14 +4,16 @@ import Todo from './Todo'
 const TodoList = props => {
 
     return (
+        <>
         <div className="todoList">
           {props.tasks.map(task => (
             <Todo key={task.id} task={task} toggleTask = {props.toggleTask}/>
           ))}
+        </div>
           <button className="clearButton" onClick={()=> {props.clearCompleted(props.tasks)}}>
             Clear Completed
           </button>
-        </div>
+        </>
       );
     };
     
